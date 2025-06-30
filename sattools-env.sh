4958abca16de3a8d75e1cc0ac8b16069b9a8e6a9
TheMouses <blanktechnolegies@gmail.com>
2025-06-30 14:28:56 +1000

    Add files via upload

---
+++ sattools-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export ST_COSPAR=9990
+export ST_DATADIR=$HOME/sattools
+export ST_TLEDIR=$HOME/tle
+export ST_OBSDIR=$HOME/satobs
+export ST_LOGIN="identity=blanktechnolegies@gmail.compassword=6Space-trackapples"
+export PATH=$ST_DATADIR/bin:$PATH
